6aac0b3d05fbe987ec00bf5974af143577e39051
Gus Ryan <guzalrya@msu.edu>
2013-08-23 13:57:36 -0400

    hole controller update

--- app/controllers/holes_controller.rb
+++ app/controllers/holes_controller.rb
@@ -3,7 +3,7 @@ class HolesController < ApplicationController
 # We need to get the post so that Rails knows where to assign the comment
 @course = Course.find(params[:course_id])
 # Here we create a new comment based on the POST data from our form (params[:comment])
-@hole = @course.holes.build(params[:hole_params])
+@hole = @course.holes.new(hole_params)
 # Once the post is saved, we redirect the user back to the post they were viewing
 redirect_to course_path(@course)
 end
