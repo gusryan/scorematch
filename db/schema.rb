c9f0f0ea582a7c726a745a9ef7954d568314d6b4
Gus Ryan <guzalrya@msu.edu>
2013-09-13 19:41:23 -0400

    misc

--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20130903210652) do
   create_table "golfers", force: true do |t|
     t.string   "name"
     t.integer  "handicap"
+    t.integer  "score"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
